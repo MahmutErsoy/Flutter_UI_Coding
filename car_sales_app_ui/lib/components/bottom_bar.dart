8fc9f8349685c21561e9ea439687fdee51e1f2d2
Mahmut Ersoy <mahmutersooy@gmail.com>
2023-07-08 18:52:13 +0300

    BottomBar Added

--- car_sales_app_ui/lib/components/bottom_bar.dart
+++ car_sales_app_ui/lib/components/bottom_bar.dart
@@ -1,4 +1,8 @@
+import 'package:car_sales_app_ui/pages/home_page.dart';
+import 'package:car_sales_app_ui/pages/search_page.dart';
+import 'package:car_sales_app_ui/style/app_style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class BottomBar extends StatefulWidget {
   const BottomBar({super.key});
@@ -8,8 +12,60 @@ class BottomBar extends StatefulWidget {
 }
 
 class _BottomBarState extends State<BottomBar> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomePage(),
+    SearchPage(),
+    Text("Index 2: Report"),
+    Text("Index 3: Notifications")
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue,
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            activeIcon:
+                SvgPicture.asset(AppStyle.homeIcon, colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn)),
+            icon: SvgPicture.asset(AppStyle.homeIcon),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            activeIcon: SvgPicture.asset(AppStyle.searchIcon,
+                colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn)),
+            icon: SvgPicture.asset(AppStyle.searchIcon),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            activeIcon: SvgPicture.asset(AppStyle.favoriteIcon,
+                colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn)),
+            icon: SvgPicture.asset(AppStyle.favoriteIcon),
+            label: "Favorite",
+          ),
+          BottomNavigationBarItem(
+            activeIcon: SvgPicture.asset(AppStyle.profileIcon,
+                colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn)),
+            icon: SvgPicture.asset(AppStyle.profileIcon),
+            label: "Profile",
+          )
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
--- car_sales_app_ui/lib/main.dart
+++ car_sales_app_ui/lib/main.dart
@@ -12,10 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: const BottomBar(),
     );
   }
